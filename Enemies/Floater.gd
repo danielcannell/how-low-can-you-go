d4f345ff54cc560657648ef5af501bdf626e420b
Jamie Wood <jamie.wood70@ntlworld.com>
2021-04-24 13:41:37 +0100

    Make floaters drift and spin

---
+++ Enemies/Floater.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var spin := 0.0
+var drift := Vector2.ZERO
+const SPIN_RATE := 1.0
+const DRIFT_RATE_X := 50.0
+const DRIFT_RATE_Y := 30.0
+
+func _ready() -> void:
+    spin = (randf() - 0.5) * SPIN_RATE
+    drift = Vector2((randf() - 0.5) * DRIFT_RATE_X, (-randf()) * DRIFT_RATE_Y)
+
+func _process(delta: float) -> void:
+    rotate(spin * delta)
+    drift = move_and_slide(drift)
