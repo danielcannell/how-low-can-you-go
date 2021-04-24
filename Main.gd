cf0812280a7011c90b312956b050b5d26ebd4e17
Daniel Cannell <danielcannell@googlemail.com>
2021-04-24 15:11:54 +0100

    Make the background get darker as you go deeper

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ const Player = preload("res://Player/Player.tscn")
 
 
 onready var camera := $Camera2D
+onready var background := $Camera2D/Background
 
 var fall_rate := 100.0
 
@@ -23,3 +24,11 @@ func spawn_player() -> void:
 func _process(delta: float) -> void:
     Globals.depth += delta * fall_rate
     camera.position.y = Globals.depth
+
+    var color_scale := exp(-Globals.depth * 1e-4)
+    background.color = color_from_hsl(0.64, 1.0 * color_scale, 0.85 * color_scale)
+
+
+func color_from_hsl(hue: float, sat: float, light: float) -> Color:
+    sat *= light if light < 0.5 else 1.0 - light
+    return Color.from_hsv(hue, 2 * sat / (light + sat), light + sat)
