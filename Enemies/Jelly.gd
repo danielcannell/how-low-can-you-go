034dc6e74b03c5a129d38af49ae4ae7bc1bfdeda
Robert McGregor <rmcgregor1990@gmail.com>
2021-04-25 15:33:39 +0100

    give jellies a light source

--- Enemies/Jelly.gd
+++ Enemies/Jelly.gd
@@ -1,5 +1,7 @@
 extends "res://Enemies/DrifterBase.gd"
 
+onready var light = $Light2D
+
 func _ready() -> void:
     spin_rate = 0
     drift_rate_x = 50.0
@@ -8,3 +10,12 @@ func _ready() -> void:
 
 func dps() -> float:
     return 5.0
+
+
+func update_light() -> void:
+    light.energy = (1 - Globals.color_scale) * 0.35
+
+
+func _process(delta):
+    if alive:
+        update_light()
