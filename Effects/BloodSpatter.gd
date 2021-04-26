32d3e917bc56bff6f07109dece7e691b6b040445
Robert McGregor <rmcgregor1990@gmail.com>
2021-04-26 20:43:08 +0100

    attempt bloodspatter fix

--- Effects/BloodSpatter.gd
+++ Effects/BloodSpatter.gd
@@ -5,11 +5,8 @@ var running = false;
 func run():
     emitting = true
     running = true
+    yield(get_tree().create_timer(4), "timeout")
+    queue_free()
 
 func set_params(params):
     amount = params['amount']
-
-
-func _process(_delta):
-    if running and not emitting:
-        queue_free()
